877c55d61959fa19b0d8e74b8173090ca65e8a8f
GitHub Action <action@github.com>
2022-04-12 17:50:07 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/milan7span/blocfilegeneratorintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.milan7span.blocfilegeneratorintellijplugin.services
+
+import com.github.milan7span.blocfilegeneratorintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
